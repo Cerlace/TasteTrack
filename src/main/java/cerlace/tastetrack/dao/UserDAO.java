a3270b92fd36ef831a66fe79933632cb17bb5515
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-01-06 22:40:11 +0300

    DAO interfaces

---
+++ src/main/java/cerlace/tastetrack/dao/UserDAO.java
@@ -0,0 +1,6 @@
+package cerlace.tastetrack.dao;
+
+import cerlace.tastetrack.entity.UserEntity;
+
+public interface UserDAO extends DAO<UserEntity> {
+}
---
+++ src/main/java/cerlace/tastetrack/dao/UserDetailsDAO.java
@@ -0,0 +1,6 @@
+package cerlace.tastetrack.dao;
+
+import cerlace.tastetrack.entity.UserDetailsEntity;
+
+public interface UserDetailsDAO extends DAO<UserDetailsEntity> {
+}
