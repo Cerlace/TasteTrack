3e13406b3a23e168fbd523ac6a95826bb9ff2928
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-01-09 17:17:57 +0300

    DishIngredient DTO and Mapper

---
+++ src/main/java/cerlace/tastetrack/dto/DishIngredientDTO.java
@@ -0,0 +1,16 @@
+package cerlace.tastetrack.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class DishIngredientDTO {
+    private DishDTO dish;
+    private IngredientDTO ingredient;
+    private Float amount;
+}
---
+++ src/main/java/cerlace/tastetrack/mapper/DishIngredientMapper.java
@@ -0,0 +1,11 @@
+package cerlace.tastetrack.mapper;
+
+import cerlace.tastetrack.dto.DishIngredientDTO;
+import cerlace.tastetrack.entity.DishIngredientEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = {DishMapper.class, IngredientMapper.class})
+public interface DishIngredientMapper extends IMapper<DishIngredientDTO, DishIngredientEntity> {
+    DishIngredientMapper INSTANCE = Mappers.getMapper(DishIngredientMapper.class);
+}
