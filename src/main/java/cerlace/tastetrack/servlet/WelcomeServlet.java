fb2ccf77513143b79d00f1c0b759f88bbc50377c
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-01-27 04:45:16 +0300

    Welcome page servlet

--- src/main/java/cerlace/tastetrack/servlet/WelcomeServlet.java
+++ src/main/java/cerlace/tastetrack/servlet/WelcomeServlet.java
@@ -1,6 +1,5 @@
 package cerlace.tastetrack.servlet;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "adminPageServlet", value = "/admin-page")
-public class AdminPageServlet extends HttpServlet {
+@WebServlet(name = "welcome", urlPatterns="/index.html")
+public class WelcomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = getServletContext()
-                .getRequestDispatcher(ServletConstants.ADMIN_PAGE_JSP);
-        requestDispatcher.forward(req, resp);
+        resp.sendRedirect(ServletConstants.USER_LIST_SERVLET);
     }
 }
