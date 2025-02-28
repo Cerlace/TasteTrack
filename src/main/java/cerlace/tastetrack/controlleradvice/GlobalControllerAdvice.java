ec2d2bbf91cbf4ea1169e3275d3da227d30f9188
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-02-28 03:20:46 +0300

    ControllerAdvice for handling DB exceptions

---
+++ src/main/java/cerlace/tastetrack/controlleradvice/GlobalControllerAdvice.java
@@ -0,0 +1,37 @@
+package cerlace.tastetrack.controlleradvice;
+
+import cerlace.tastetrack.dto.AlertDTO;
+import cerlace.tastetrack.enums.AlertCode;
+import cerlace.tastetrack.enums.AlertMessage;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    /**
+     * Обрабатывает исключение {@link SQLIntegrityConstraintViolationException}, которое возникает
+     * при нарушении целостности данных в базе данных (например, при попытке вставить дублирующиеся
+     * данные).
+     *
+     * <p>Добавляет атрибут "alert" в {@link RedirectAttributes}, который содержит информацию
+     * о предупреждении, и перенаправляет пользователя на предыдущую страницу.</p>
+     *
+     * @param request            объект {@link WebRequest}, содержащий информацию о запросе.
+     * @param redirectAttributes объект {@link RedirectAttributes}, используемый для передачи
+     *                           атрибутов между запросами.
+     * @return перенаправление на предыдущую страницу.
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public String handleConstraintViolation(WebRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("alert", AlertDTO.builder()
+                .alertCode(AlertCode.WARNING)
+                .alertMessage(AlertMessage.CONSTRAINT)
+                .build());
+
+        return "redirect:" + request.getHeader("referer");
+    }
+}
