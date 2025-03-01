e819b2f20980a1665fa6ee7990b84062f642c7c1
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-03-01 19:03:06 +0300

    Repositories update for pagination

--- src/main/java/cerlace/tastetrack/repository/DishIngredientRepository.java
+++ src/main/java/cerlace/tastetrack/repository/DishIngredientRepository.java
@@ -1,6 +1,8 @@
 package cerlace.tastetrack.repository;
 
 import cerlace.tastetrack.entity.DishIngredientEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +12,18 @@ import java.util.List;
 public interface DishIngredientRepository extends JpaRepository<DishIngredientEntity, Long> {
     /**
      * Получает все {@code DishIngredientEntity}, которые относятся к блюду
+     *
      * @param dishId идентификатор блюда
      * @return список ингредиентов в формате {@code List<DishIngredientEntity>}
      */
-    List<DishIngredientEntity> getAllByDishId(Long dishId);
+    List<DishIngredientEntity> findByDishId(Long dishId);
+
+    /**
+     * Получает страницу {@code DishIngredientEntity}, которые относятся к блюду
+     *
+     * @param pageable запрос на получение страницы.
+     * @param dishId   идентификатор блюда.
+     * @return список приемов пищи в формате {@code List<MealEntity>}
+     */
+    Page<DishIngredientEntity> findByDishId(Pageable pageable, Long dishId);
 }
--- src/main/java/cerlace/tastetrack/repository/MealRepository.java
+++ src/main/java/cerlace/tastetrack/repository/MealRepository.java
@@ -1,6 +1,8 @@
 package cerlace.tastetrack.repository;
 
 import cerlace.tastetrack.entity.MealEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +12,16 @@ import java.util.List;
 public interface MealRepository extends JpaRepository<MealEntity, Long> {
     /**
      * Получает все {@code MealEntity}, которые относятся к пользователю
-     * @param userId идентификатор пользователя
+     * @param userId идентификатор пользователя.
      * @return список приемов пищи в формате {@code List<MealEntity>}
      */
-    List<MealEntity> getAllByUserId(Long userId);
+    List<MealEntity> findByUserId(Long userId);
+
+    /**
+     * Получает страницу {@code MealEntity}, которые относятся к пользователю
+     * @param pageable запрос на получение страницы.
+     * @param userId идентификатор пользователя.
+     * @return список приемов пищи в формате {@code List<MealEntity>}
+     */
+    Page<MealEntity> findByUserId(Pageable pageable, Long userId);
 }
