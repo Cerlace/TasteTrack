348f905c8bf673706587147caa958c522699ebfe
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-03-03 02:16:35 +0300

    UserService saveOrUpdate with password and passwordCrypt bean

---
+++ src/main/java/cerlace/tastetrack/configuration/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package cerlace.tastetrack.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig {
+    /**
+     * Создаёт и возвращает экземпляр {@link BCryptPasswordEncoder} для шифрования паролей.
+     *
+     * @return экземпляр {@link BCryptPasswordEncoder}.
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/cerlace/tastetrack/service/impl/UserServiceImpl.java
+++ src/main/java/cerlace/tastetrack/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import cerlace.tastetrack.dto.PageSettings;
 import cerlace.tastetrack.dto.UserDTO;
 
 import cerlace.tastetrack.entity.UserEntity;
+import cerlace.tastetrack.exception.PasswordConfirmException;
 import cerlace.tastetrack.mapper.UserMapper;
 import cerlace.tastetrack.repository.UserRepository;
 import cerlace.tastetrack.service.UserService;
@@ -12,19 +13,29 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    public static final String PASSWORD_CONFIRM_EXCEPTION_MESSAGE = "Password and confirm password don't match!";
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDTO saveOrUpdate(UserDTO dto) {
+        if (!Objects.equals(dto.getPassword(), dto.getPasswordConfirm())) {
+            throw new PasswordConfirmException(PASSWORD_CONFIRM_EXCEPTION_MESSAGE);
+        }
+        if (dto.getPassword() != null && !dto.getPassword().isBlank()) {
+            dto.setEncodedPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+        }
         UserEntity entity = mapper.toEntity(dto);
         return mapper.toDTO(repository.save(entity));
     }
