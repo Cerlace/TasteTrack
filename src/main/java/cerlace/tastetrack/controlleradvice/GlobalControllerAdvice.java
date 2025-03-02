a9d2a47b22664cd9545cb1187973bc646ef029e6
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-03-03 02:18:37 +0300

    PasswordConfirmException handling

--- src/main/java/cerlace/tastetrack/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/cerlace/tastetrack/controlleradvice/GlobalControllerAdvice.java
@@ -3,6 +3,7 @@ package cerlace.tastetrack.controlleradvice;
 import cerlace.tastetrack.dto.AlertDTO;
 import cerlace.tastetrack.enums.AlertCode;
 import cerlace.tastetrack.enums.AlertMessage;
+import cerlace.tastetrack.exception.PasswordConfirmException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -30,6 +31,28 @@ public class GlobalControllerAdvice {
         redirectAttributes.addFlashAttribute("alert", AlertDTO.builder()
                 .alertCode(AlertCode.WARNING)
                 .alertMessage(AlertMessage.CONSTRAINT)
+                .build());
+
+        return "redirect:" + request.getHeader("referer");
+    }
+
+    /**
+     * Обрабатывает исключение {@link PasswordConfirmException}, которое возникает
+     * при неудачном подтверждении пароля в форме регистрации или смены пароля.
+     *
+     * <p>Добавляет атрибут "alert" в {@link RedirectAttributes}, который содержит информацию
+     * о предупреждении, и перенаправляет пользователя на предыдущую страницу.</p>
+     *
+     * @param request            объект {@link WebRequest}, содержащий информацию о запросе.
+     * @param redirectAttributes объект {@link RedirectAttributes}, используемый для передачи
+     *                           атрибутов между запросами.
+     * @return перенаправление на предыдущую страницу.
+     */
+    @ExceptionHandler(PasswordConfirmException.class)
+    public String handlePasswordConfirmException(WebRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("alert", AlertDTO.builder()
+                .alertCode(AlertCode.WARNING)
+                .alertMessage(AlertMessage.PASSWORD_CONFIRM)
                 .build());
 
         return "redirect:" + request.getHeader("referer");
