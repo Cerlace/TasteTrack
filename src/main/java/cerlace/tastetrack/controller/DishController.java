5b6a610245309f3ca7a47282fb6d88a6e2ca7214
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-03-03 21:50:09 +0300

    Filter update for controller and page

--- src/main/java/cerlace/tastetrack/controller/DishController.java
+++ src/main/java/cerlace/tastetrack/controller/DishController.java
@@ -3,11 +3,13 @@ package cerlace.tastetrack.controller;
 import cerlace.tastetrack.dto.AlertDTO;
 import cerlace.tastetrack.dto.DishDTO;
 import cerlace.tastetrack.dto.DishFilter;
+import cerlace.tastetrack.dto.IngredientDTO;
 import cerlace.tastetrack.dto.PageSettings;
 import cerlace.tastetrack.enums.AlertCode;
 import cerlace.tastetrack.enums.AlertMessage;
 import cerlace.tastetrack.enums.DishType;
 import cerlace.tastetrack.service.DishService;
+import cerlace.tastetrack.service.IngredientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,12 +22,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/dishes")
 public class DishController {
 
     private final DishService dishService;
+    private final IngredientService ingredientService;
+
+    /**
+     * Добавляет список всех ингредиентов в модель.
+     *
+     * @return список всех ингредиентов в виде {@link List < IngredientDTO >}.
+     */
+    @ModelAttribute("ingredientsList")
+    public List<IngredientDTO> addIngredientsToModel() {
+        return ingredientService.getAll();
+    }
 
     /**
      * Отображает страницу блюд.
