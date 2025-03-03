e967d0f169421009b04bc13e96bf60c568709a92
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-03-04 00:41:55 +0300

    Mapper update for dishes

--- src/main/java/cerlace/tastetrack/mapper/DishIngredientMapper.java
+++ src/main/java/cerlace/tastetrack/mapper/DishIngredientMapper.java
@@ -5,7 +5,18 @@ import cerlace.tastetrack.entity.DishIngredientEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.Set;
+
 @Mapper(componentModel = "spring", uses = {DishMapper.class, IngredientMapper.class})
 public interface DishIngredientMapper extends IMapper<DishIngredientDTO, DishIngredientEntity> {
     DishIngredientMapper INSTANCE = Mappers.getMapper(DishIngredientMapper.class);
+
+    /**
+     * Метод конвертирует набор объектов {@code Entity} в
+     * набор объектов {@code DTO}
+     *
+     * @param entitySet входной набор объектов типа {@code Entity}
+     * @return набор объектов типа {@code DTO}
+     */
+    Set<DishIngredientDTO> toDTOSet(Set<DishIngredientEntity> entitySet);
 }
--- src/main/java/cerlace/tastetrack/mapper/DishMapper.java
+++ src/main/java/cerlace/tastetrack/mapper/DishMapper.java
@@ -3,9 +3,16 @@ package cerlace.tastetrack.mapper;
 import cerlace.tastetrack.dto.DishDTO;
 import cerlace.tastetrack.entity.DishEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface DishMapper extends IMapper<DishDTO, DishEntity> {
     DishMapper INSTANCE = Mappers.getMapper(DishMapper.class);
+
+    @Mapping(target = "dishIngredients", ignore = true)
+    DishDTO toDTO(DishEntity entity);
+
+    @Mapping(target = "dishIngredients", ignore = true)
+    DishEntity toEntity(DishDTO dto);
 }
