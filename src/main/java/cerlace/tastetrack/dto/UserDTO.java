a89355867f255a52d16580b9d0874cae83f3bff5
Cerlace <124503215+Cerlace@users.noreply.github.com>
2025-03-10 23:16:48 +0300

    Constants for user validation

--- src/main/java/cerlace/tastetrack/dto/UserDTO.java
+++ src/main/java/cerlace/tastetrack/dto/UserDTO.java
@@ -21,13 +21,18 @@ import java.util.Set;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class UserDTO extends BaseDTO {
-    @Size(min = 3, max = 16)
+    public static final int USERNAME_MIN_SIZE = 3;
+    public static final int USERNAME_MAX_SIZE = 16;
+    public static final int FULL_NAME_MIN_SIZE = 3;
+    public static final int FILL_NAME_MAX_SIZE = 32;
+
+    @Size(min = USERNAME_MIN_SIZE, max = USERNAME_MAX_SIZE)
     @Pattern(regexp = "^[A-Za-z0-9]*$")
     private String username;
     private String oldPassword;
     private String password;
     private String passwordConfirm;
-    @Size(min = 3, max = 32)
+    @Size(min = FULL_NAME_MIN_SIZE, max = FILL_NAME_MAX_SIZE)
     @Pattern(regexp = "^\\p{Lu}\\p{L}*(\\s\\p{Lu}\\p{L}*)*$")
     private String fullName;
     @Past
